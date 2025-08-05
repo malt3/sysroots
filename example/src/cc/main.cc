f3d20a8483e3855d18c4178d081c28e8b8597412
Malte Poll <1780588+malt3@users.noreply.github.com>
2025-08-05 22:07:56 +0200

    update example to use working C++ standard library headers

--- example/src/cc/main.cc
+++ example/src/cc/main.cc
@@ -1,9 +1,10 @@
+#include <iostream>
 #include "src/cc/foo.h"
 
 int main() {
     int x = 5;
     int y = 10;
     int sum = add(x, y);
+    std::cout << "The sum of " << x << " and " << y << " is: " << sum << std::endl;
     return sum;
 }
-
